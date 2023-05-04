a0c749a7b12b8dc254de79281cdca17dbc5c87af
Atp1337 <132529149+Atp1337@users.noreply.github.com>
2023-05-03 20:48:43 -0600

    Update Prueba.java

--- src/Prueba.java
+++ src/Prueba.java
@@ -13,8 +13,7 @@ public class Prueba {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+
+    
     
 }
