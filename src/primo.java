8c6e9762f1315987b7441d22d9fcc658b63cd6ed
Atp1337 <132529149+Atp1337@users.noreply.github.com>
2023-05-04 20:58:18 -0600

    TicTimer.java

--- src/primo.java
+++ src/primo.java
@@ -1,3 +1,41 @@
-public class primo {
-    //Prueba prueba
+package prueba;
+import java.awt.Color; 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.Timer;
+
+public class TicTimer extends JFrame {
+  boolean flag = false;
+
+  JButton button = new JButton("Click to stop");
+
+  Timer timer;
+
+  public TicTimer() {
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    getContentPane().add(button, "Center");
+    button.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        timer.stop();
+        button.setBackground(Color.red);
+      }
+    });
+    timer = new Timer(1000, new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        button.setBackground(flag ? Color.green : Color.yellow);
+        flag = !flag;
+        repaint();
+      }
+    });
+    timer.start();
+    pack();
+    setVisible(true);
+  }
+
+  public static void main(String arg[]) {
+    new TicTimer();
+  }
 }
